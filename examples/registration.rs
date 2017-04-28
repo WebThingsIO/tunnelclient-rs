adcc56543e030068e48dd653692f7db42c326fef
Fabrice Desré <fabrice@desre.org>
2017-04-28 14:46:25 -0700

    Initial release

---
+++ examples/registration.rs
@@ -0,0 +1,174 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+extern crate env_logger;
+extern crate get_if_addrs;
+#[macro_use]
+extern crate log;
+extern crate pagekite;
+extern crate tunnelclient;
+
+use get_if_addrs::{IfAddr, Interface};
+use pagekite::{PageKite, InitFlags, LOG_NORMAL};
+use std::env;
+use std::fs::File;
+use std::io::Error;
+use std::io::prelude::*;
+use std::thread;
+use std::time::Duration;
+use tunnelclient::TunnelClient;
+
+const HOST: &'static str = "http://knilxof.org";
+const DOMAIN: &'static str = "knilxof.org";
+const DEFAULT_USER: &'static str = "demo";
+const TUNNEL_SECRET: &'static str = "moziot";
+const TUNNEL_PORT: i32 = 4443;
+const LOCAL_PORT: i32 = 4443;
+
+fn get_saved_token() -> Result<String, Error> {
+    let mut file = File::open("tunnel_token")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn get_ip_addr_from_ifaces(ifaces: &[Interface], want_iface: &Option<String>) -> Option<String> {
+
+    let mut ip_addr: Option<String> = None;
+    let mut ipv6_addr: Option<String> = None;
+
+    for iface in ifaces {
+        match want_iface.as_ref() {
+                None =>
+                    // Whitelist known good iface
+                    if !(iface.name.starts_with("eth") ||
+                         iface.name.starts_with("wlan") ||
+                         iface.name.starts_with("en") ||
+                         iface.name.starts_with("em") ||
+                         iface.name.starts_with("wlp3s") ||
+                         iface.name.starts_with("wlp4s")) {
+                        continue;
+                    },
+                    Some(iface_name) =>
+                        if &iface.name != iface_name {
+                            continue;
+                        }
+            }
+        if let IfAddr::V4(ref v4) = iface.addr {
+            ip_addr = Some(format!("{}", v4.ip));
+            break;
+        } else if ipv6_addr.is_none() {
+            if let IfAddr::V6(ref v6) = iface.addr {
+                ipv6_addr = Some(format!("{}", v6.ip));
+            }
+        }
+    }
+
+    if ip_addr.is_none() {
+        if ipv6_addr.is_none() {
+            error!("No IP interfaces found!");
+        } else {
+            ip_addr = ipv6_addr;
+        }
+    }
+    ip_addr
+}
+
+/// return the host IP address of the first valid interface.
+/// `want_iface` is an options string for the interface you want.
+pub fn get_ip_addr(want_iface: &Option<String>) -> Option<String> {
+    // Look for an ipv4 interface on eth* or wlan*.
+    if let Ok(ifaces) = get_if_addrs::get_if_addrs() {
+        if ifaces.is_empty() {
+            error!("No IP interfaces found!");
+            return None;
+        }
+
+        get_ip_addr_from_ifaces(&ifaces, want_iface)
+    } else {
+        error!("No IP interfaces found!");
+        None
+    }
+}
+
+fn main() {
+    env_logger::init().unwrap();
+
+    // if we have a command line parameter, use it as a name to register.
+    let user_name = match env::args().nth(1) {
+        Some(name) => name,
+        None => DEFAULT_USER.to_owned(),
+    };
+
+    info!("Starting registration for {} ...", user_name);
+
+    // Check if we can get a saved token
+    let token = match get_saved_token() {
+        Ok(token) => token,
+        Err(_) => {
+            let client = TunnelClient::new(HOST, None, None);
+            let client = client
+                .subscribe(&user_name)
+                .expect("Failed to subscribe!");
+            let token = client.token.unwrap();
+            info!("New client token is {}", token);
+            let mut file = File::create("tunnel_token").unwrap();
+            file.write_all(token.as_bytes()).unwrap();
+            token
+        }
+    };
+
+    // Check if we have cert.pem and key.pem and trigger the Let's Encrypt
+    // flow if not.
+    if File::open("certificate.pem").is_err() || File::open("privatekey.pem").is_err() {
+        let client = TunnelClient::new(HOST, Some(token.clone()), Some(user_name.to_owned()));
+        client
+            .lets_encrypt(DOMAIN)
+            .expect("Failed to complete the DNS challenge");
+    } else {
+        info!("We have a certificate and a key already, skipping Let's Encrypt challenge.");
+    }
+
+    // Register the endpoint every minute to keep our record up to date.
+
+    let local_ip = get_ip_addr(&None).expect("Failed to get the local ip address!");
+
+    info!("local ip is {}", local_ip);
+
+    let u_name = user_name.clone();
+    let handle = thread::Builder::new()
+        .name("eviction".into())
+        .spawn(move || {
+            let delay = 60;
+            info!("Starting registration thread, delay is {}s", delay);
+            loop {
+                thread::sleep(Duration::new(delay as u64, 0));
+                let client = TunnelClient::new(HOST, Some(token.clone()), Some(u_name.clone()));
+                info!("Registering `{}.box.{}` with {}", u_name, DOMAIN, local_ip);
+                // Ignore errors that could be transient.
+                client.register(&local_ip).unwrap_or(());
+            }
+        })
+        .expect("Failed to start eviction thread!");
+
+    // Setup the PageKite tunnel.
+    let pagekite = PageKite::init(Some("moziot-tunnel"),
+                                  1, // max kites: just one for https.
+                                  1, // max frontends
+                                  10, // max connections.
+                                  None, // dyndns url
+                                  &[InitFlags::WithIpv4, InitFlags::WithIpv6],
+                                  &LOG_NORMAL)
+            .expect("Failed to create PageKite object!");
+    pagekite.lookup_and_add_frontend(DOMAIN, TUNNEL_PORT, true);
+    pagekite.add_kite("https",
+                      &format!("{}.box.{}", user_name, DOMAIN),
+                      TUNNEL_PORT,
+                      TUNNEL_SECRET,
+                      "localhost",
+                      LOCAL_PORT);
+    pagekite.thread_start();
+
+    handle.join().unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,175 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+extern crate acme_client;
+#[macro_use]
+extern crate log;
+extern crate reqwest;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+use acme_client::Directory;
+use acme_client::error::Error as AcmeError;
+use reqwest::{Client, StatusCode};
+
+pub struct TunnelClient {
+    pub tunnel_url: String,
+    pub token: Option<String>,
+    pub name: Option<String>,
+}
+
+#[derive(Debug)]
+pub enum TunnelClientError {
+    NoName,
+    NoToken,
+    NoChallenge,
+    BadRequest,
+    Other(String),
+    Acme(AcmeError),
+}
+
+impl std::convert::From<AcmeError> for TunnelClientError {
+    fn from(err: AcmeError) -> Self {
+        TunnelClientError::Acme(err)
+    }
+}
+
+impl std::convert::From<reqwest::Error> for TunnelClientError {
+    fn from(err: reqwest::Error) -> Self {
+        TunnelClientError::Other(format!("{}", err))
+    }
+}
+
+impl TunnelClient {
+    pub fn new(tunnel_url: &str, token: Option<String>, name: Option<String>) -> Self {
+        TunnelClient {
+            tunnel_url: tunnel_url.to_owned(),
+            token: token,
+            name: name,
+        }
+    }
+
+    // Triggers a subscription.
+    pub fn subscribe(&self, name: &str) -> Option<Self> {
+        let client = Client::new().expect("Client creation failure");
+        match client
+                  .get(&format!("{}/subscribe?name={}", self.tunnel_url, name))
+                  .send() {
+
+            // If the status is 200, the response is {"name": "xxx", "token": "yyy"}
+            Ok(mut response) => {
+                if *response.status() == StatusCode::Ok {
+                    #[derive(Deserialize)]
+                    struct NameAndToken {
+                        name: String,
+                        token: String,
+                    }
+                    let data: Result<NameAndToken, reqwest::Error> = response.json();
+                    match data {
+                        Ok(n_t) => {
+                            Some(TunnelClient {
+                                     tunnel_url: self.tunnel_url.clone(),
+                                     token: Some(n_t.token),
+                                     name: Some(n_t.name),
+                                 })
+                        }
+                        Err(_) => None,
+                    }
+                } else {
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
+
+    // Renew the registration with a given local ip.
+    // Will return false if this failed for any reason.
+    pub fn register(&self, local_ip: &str) -> Result<(), TunnelClientError> {
+        if let Some(ref token) = self.token {
+            let client = Client::new().expect("Client creation failure");
+            return match client
+                             .get(&format!("{}/register?token={}&local_ip={}",
+                                           self.tunnel_url,
+                                           token,
+                                           local_ip))
+                             .send() {
+                       Ok(response) => {
+                           if *response.status() == StatusCode::Ok {
+                               Ok(())
+                           } else {
+                               Err(TunnelClientError::BadRequest)
+                           }
+                       }
+                       Err(err) => Err(TunnelClientError::from(err)),
+                   };
+        } else {
+            error!("No token available to register {}", local_ip);
+            return Err(TunnelClientError::NoToken);
+        }
+    }
+
+    // Starts the LE workflow.
+    pub fn lets_encrypt(&self, domain: &str) -> Result<(), TunnelClientError> {
+        let name = if let Some(ref name) = self.name {
+            name
+        } else {
+            error!("Can't run lets_encrypt without a name");
+            return Err(TunnelClientError::NoName);
+        };
+
+        if let Some(ref token) = self.token {
+
+            let domain = format!("{}.box.{}", name, domain);
+            let directory = Directory::lets_encrypt()?;
+            let account = directory.account_registration().register()?;
+
+            let authorization = account.authorization(&domain)?;
+            let dns_challenge = match authorization.get_dns_challenge() {
+                Some(challenge) => challenge,
+                None => return Err(TunnelClientError::NoChallenge),
+            };
+            let signature = dns_challenge.signature()?;
+
+            let client = Client::new().expect("Client creation failure");
+            client
+                .get(&format!("{}/dnsconfig?token={}&challenge={}",
+                              self.tunnel_url,
+                              token,
+                              signature))
+                .send()?;
+
+            dns_challenge.validate()?;
+            info!("DNS challenge validated for {}", domain);
+
+            let domain_str = domain.as_str();
+            let domains = [domain_str];
+            let certificate_signer = account.certificate_signer(&domains);
+            let cert = certificate_signer.sign_certificate()?;
+            cert.save_signed_certificate("certificate.pem")?;
+            cert.save_private_key("privatekey.pem")?;
+            info!("Certificate and private key for {} saved.", domain);
+            return Ok(());
+        } else {
+            error!("No token available to retrieve the certificate for {}",
+                   domain);
+            return Err(TunnelClientError::NoToken);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::TunnelClient;
+
+    #[test]
+    fn test_api() {
+        let client = TunnelClient::new("http://localhost:4242", None, None);
+        let client = client.subscribe("fabrice").unwrap();
+        assert_eq!(client.name, Some("fabrice".to_owned()));
+        assert_eq!(client.token.is_some(), true);
+    }
+}
