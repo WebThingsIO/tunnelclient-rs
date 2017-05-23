add5da90e79f1f149ddab14868f98199bb101bcb
Fabrice Desré <fabrice@desre.org>
2017-05-23 15:28:37 -0700

    Properly deal with empty responses

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ use acme_client::error::Error as AcmeError;
 use registration_server::api_types::{Discovered, NameAndToken, ServerInfo};
 use url::percent_encoding::{percent_encode, QUERY_ENCODE_SET};
 use reqwest::{Client, StatusCode};
+use std::convert::From;
 
 pub struct TunnelClient {
     pub tunnel_url: String,
@@ -37,19 +38,19 @@ fn url_param(param: &str) -> String {
     percent_encode(param.as_bytes(), QUERY_ENCODE_SET).collect::<String>()
 }
 
-impl std::convert::From<AcmeError> for TunnelClientError {
+impl From<AcmeError> for TunnelClientError {
     fn from(err: AcmeError) -> Self {
         TunnelClientError::Acme(err)
     }
 }
 
-impl std::convert::From<reqwest::Error> for TunnelClientError {
+impl From<reqwest::Error> for TunnelClientError {
     fn from(err: reqwest::Error) -> Self {
         TunnelClientError::Other(format!("{}", err))
     }
 }
 
-// Macro that helps with declaring API endpoints.
+// Macros that helps with declaring API endpoints.
 macro_rules! api_endpoint {
     ($name:ident, $base:expr, $with_token:expr, $ret:ty) => (
         pub fn $name(&self, params: &Vec<(&str, Option<&str>)>) -> Result<$ret, TunnelClientError> {
@@ -66,13 +67,39 @@ macro_rules! api_endpoint {
                     .send() {
                 Ok(mut response) => {
                     if *response.status() == StatusCode::Ok {
-                        let info: Result<$ret, reqwest::Error> = response.json();
-                        match info {
-                            Ok(info) => {
-                                Ok(info)
-                            }
+                        let res: Result<$ret, reqwest::Error> = response.json();
+                        match res {
+                            Ok(res) => Ok(res),
                             Err(err) => Err(TunnelClientError::from(err)),
                         }
+                    } else {
+                        Err(TunnelClientError::BadRequest)
+                    }
+                }
+                Err(err) => Err(TunnelClientError::from(err)),
+            }
+        }
+    )
+}
+
+// Special case for empty answers.
+macro_rules! empty_api_endpoint {
+    ($name:ident, $base:expr, $with_token:expr) => (
+        pub fn $name(&self, params: &Vec<(&str, Option<&str>)>) -> Result<(), TunnelClientError> {
+            if $with_token {
+                if self.token.is_none() {
+                    error!("No token available!");
+                    return Err(TunnelClientError::NoToken);
+                }
+            }
+
+            let client = Client::new().expect("Client creation failure");
+            match client
+                    .get(&self.get_full_url($base, params, $with_token))
+                    .send() {
+                Ok(response) => {
+                    if *response.status() == StatusCode::Ok {
+                        Ok(())
                     } else {
                         Err(TunnelClientError::BadRequest)
                     }
@@ -129,17 +156,17 @@ impl TunnelClient {
         }
     }
 
-    api_endpoint!(call_unsubscribe, "unsubscribe", true, ());
+    empty_api_endpoint!(call_unsubscribe, "unsubscribe", true);
     pub fn unsubscribe(&self) -> Result<(), TunnelClientError> {
         self.call_unsubscribe(&vec![])
     }
 
-    api_endpoint!(call_register, "register", true, ());
+    empty_api_endpoint!(call_register, "register", true);
     pub fn register(&self, local_ip: &str) -> Result<(), TunnelClientError> {
         self.call_register(&vec![("local_ip", Some(local_ip))])
     }
 
-    api_endpoint!(call_dnsconfig, "dnsconfig", true, ());
+    empty_api_endpoint!(call_dnsconfig, "dnsconfig", true);
     pub fn dnsconfig(&self, challenge: &str) -> Result<(), TunnelClientError> {
         self.call_dnsconfig(&vec![("challenge", Some(challenge))])
     }
@@ -154,22 +181,22 @@ impl TunnelClient {
         self.call_ping(&vec![])
     }
 
-    api_endpoint!(call_adddiscovery, "adddiscovery", true, ());
+    empty_api_endpoint!(call_adddiscovery, "adddiscovery", true);
     pub fn adddiscovery(&self, disco: &str) -> Result<(), TunnelClientError> {
         self.call_adddiscovery(&vec![("disco", Some(disco))])
     }
 
-    api_endpoint!(call_revokediscovery, "adddiscovery", true, ());
+    empty_api_endpoint!(call_revokediscovery, "adddiscovery", true);
     pub fn revokediscovery(&self, disco: &str) -> Result<(), TunnelClientError> {
         self.call_revokediscovery(&vec![("disco", Some(disco))])
     }
 
-    api_endpoint!(call_setemail, "setemail", true, ());
+    empty_api_endpoint!(call_setemail, "setemail", true);
     pub fn setemail(&self, email: &str) -> Result<(), TunnelClientError> {
         self.call_setemail(&vec![("email", Some(email))])
     }
 
-    api_endpoint!(call_revokeemail, "revokeemail", true, ());
+    empty_api_endpoint!(call_revokeemail, "revokeemail", true);
     pub fn revokeemail(&self, email: &str) -> Result<(), TunnelClientError> {
         self.call_revokeemail(&vec![("email", Some(email))])
     }
@@ -183,46 +210,39 @@ impl TunnelClient {
             return Err(TunnelClientError::NoName);
         };
 
-        if let Some(ref token) = self.token {
-
-            let directory = Directory::lets_encrypt()?;
-            let account = directory.account_registration().register()?;
-
-            let remote_domain = format!("{}.box.{}", name, domain);
-            let local_domain = format!("local.{}.box.{}", name, domain);
-
-            let domains = [remote_domain.as_str(), local_domain.as_str()];
-
-            for domain in domains.iter() {
-                let authorization = account.authorization(&domain)?;
-                let dns_challenge = match authorization.get_dns_challenge() {
-                    Some(challenge) => challenge,
-                    None => return Err(TunnelClientError::NoChallenge),
-                };
-                let signature = dns_challenge.signature()?;
-
-                let client = Client::new().expect("Client creation failure");
-                client
-                    .get(&format!("{}/dnsconfig?token={}&challenge={}",
-                                 self.tunnel_url,
-                                 url_param(token),
-                                 url_param(&signature)))
-                    .send()?;
-
-                dns_challenge.validate()?;
-                info!("DNS challenge validated for {}", domain);
-            }
-
-            let certificate_signer = account.certificate_signer(&domains);
-            let cert = certificate_signer.sign_certificate()?;
-            cert.save_signed_certificate_and_chain(None, "certificate.pem")?;
-            cert.save_private_key("privatekey.pem")?;
-            info!("Certificate and private key for {} saved.", domain);
-            return Ok(());
-        } else {
+        if self.token.is_none() {
             error!("No token available to retrieve the certificate for {}",
                    domain);
             return Err(TunnelClientError::NoToken);
         }
+
+        let directory = Directory::lets_encrypt()?;
+        let account = directory.account_registration().register()?;
+
+        let remote_domain = format!("{}.box.{}", name, domain);
+        let local_domain = format!("local.{}.box.{}", name, domain);
+
+        let domains = [remote_domain.as_str(), local_domain.as_str()];
+
+        for domain in domains.iter() {
+            let authorization = account.authorization(&domain)?;
+            let dns_challenge = match authorization.get_dns_challenge() {
+                Some(challenge) => challenge,
+                None => return Err(TunnelClientError::NoChallenge),
+            };
+            let signature = dns_challenge.signature()?;
+
+            self.dnsconfig(&signature)?;
+
+            dns_challenge.validate()?;
+            info!("DNS challenge validated for {}", domain);
+        }
+
+        let certificate_signer = account.certificate_signer(&domains);
+        let cert = certificate_signer.sign_certificate()?;
+        cert.save_signed_certificate_and_chain(None, "certificate.pem")?;
+        cert.save_private_key("privatekey.pem")?;
+        info!("Certificate and private key for {} saved.", domain);
+        Ok(())
     }
 }
